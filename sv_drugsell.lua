0349ddb9fada4e122a72a8cad95998cac90c1ac3
cadburry <72443203+cadburry6969@users.noreply.github.com>
2022-05-20 21:51:41 +0530

    0x2F7A49E6 fix

--- sv_drugsell.lua
+++ sv_drugsell.lua
@@ -1,14 +1,15 @@
 local QBCore = exports[Config.Core]:GetCoreObject()
 
 RegisterNetEvent('cad-drugsales:initiatedrug', function(ZoneDrug)	
-	local Player = QBCore.Functions.GetPlayer(source)
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
 	if Player then		
 		local price = ZoneDrug.price				
 		Player.Functions.RemoveItem(ZoneDrug.item, 1)
-       	TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items[ZoneDrug.item], "remove", 1)		
+       	TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[ZoneDrug.item], "remove", 1)		
 		Wait(500) -- Just to make sure the item is removed and them money is provided to the player		
 		Player.Functions.AddMoney("cash", price)
-		TriggerClientEvent('cad-drugsales:notify', source, 'You recieved $'..price)
+		TriggerClientEvent('cad-drugsales:notify', src, 'You recieved $'..price)
 		if Config.Debug then print('You got 1 '..ZoneDrug.item..' for $'..price) end
 	end
 end)
