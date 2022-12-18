1fd64e886c12a9cc26ab23d0990f112db57d70cc
cadburry <archxcadburry@gmail.com>
2022-12-18 22:44:06 +0530

    feat: Config.GiveBonusOnPolice has some use now

--- sv_drugsell.lua
+++ sv_drugsell.lua
@@ -11,11 +11,34 @@ QBCore.Functions.CreateCallback('cad-drugsales:server:GetCops', function(source,
     cb(amount)
 end)
 
+function GetDeliveryCops()
+    local amount = 0
+    for k, v in pairs(QBCore.Functions.GetPlayers()) do
+        local Player = QBCore.Functions.GetPlayer(v)
+        if Player ~= nil then
+            if Player.PlayerData.job.name == "police" and Player.PlayerData.job.onduty then
+                amount = amount + 1
+            end
+        end
+	end
+    return amount
+end
+
 RegisterNetEvent('cad-drugsales:initiatedrug', function(cad)	
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
 	if Player then
 		local price = math.floor(cad.price * cad.amt)
+		if Config.GiveBonusOnPolice then
+			local copsamount = GetDeliveryCops()
+			if copsamount > 0 and copsamount < 3 then
+				price = price * 1.2
+			elseif copsamount >= 3 and copsamount <= 6 then
+				price = price * 1.5
+			elseif copsamount >= 7 and copsamount <= 10 then
+				price = price * 2.0            
+			end
+		end
 		if Player.Functions.GetItemByName(tostring(cad.item)) then
 			if Player.Functions.RemoveItem(tostring(cad.item), cad.amt) then
 				TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[tostring(cad.item)], "remove", cad.amt)
